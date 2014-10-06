3648fb7350bceddd65d07b95e57c7adb5351c1ed
Pedro Silva <pedro.cls93@gmail.com>
2014-10-06 11:58:53 +0100

    First 2 ajax fully functional!

--- server.js
+++ server.js
@@ -11,18 +11,29 @@ var bodyParser = require('body-parser');
 var request = require('request');			//for proxy http requests
 var request = request.defaults({jar: true}) //Store cookies
 
+var http_session='';
+var si_session='';
+var si_security='';
+
 // Set the headers
 var headers = {
     'User-Agent':       'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36',
-    'Content-Type':     'text/plain;charset=UTF-16'
+    'Content-Type':     'text/plain;charset=UTF-16',
+	'set-cookie' : http_session+si_session+si_security
 }
 
 // Configure the checklogin request
 var loginCredentials = {
-    url: 'http://sigarra.up.pt/feup/pt/vld_validacao.validacao',
+    url: 'https://sigarra.up.pt/feup/pt/vld_validacao.validacao',
     method: 'POST',
     headers: headers,
     form: {'p_app': '162', 'p_amo': '1665', 'p_user': '', 'p_pass': ''}
+}
+
+var initialWebPage = {
+    url: 'https://sigarra.up.pt/feup/pt/WEB_PAGE.INICIAL',
+    method: 'GET',
+    headers: headers
 }
 
 var getStudentPage = {
@@ -53,32 +64,51 @@ router.get('/',function(req,res){
   res.sendfile(__dirname + '/public/index.html');
 })
 
-router.get('/userpage', function(req, res) {	
-	getStudentPage.form.pct_id = req.body.userid;
-	// Start the request	
-	request(getStudentPage, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			res.json({body: response});
-		}
-		else{
-			res.json({ response: 400, body: response});
-		}
-	})
-});
-
-router.post('/login', function(req, res) {
+router.post('/api/login', function(req, res) {
 	
 	loginCredentials.form.p_user = req.body.username;
 	loginCredentials.form.p_pass = req.body.password;
 	
 	// Start the request	
 	request(loginCredentials, function (error, response) {
-		if (!error && response.statusCode == 200 && response.headers["set-cookie"][1]!== undefined) {
-				
-				res.json({response: 200, body: response});
-				
+		if (!error && response.statusCode == 200 && response.headers["set-cookie"][1]!== undefined) {				
+						
+			si_session = response.headers["set-cookie"][0];
+			si_security = response.headers["set-cookie"][1];
+			
+			headers["set-cookie"] = "";
+			headers["set-cookie"] = http_session+si_session+si_security;
+			res.send(response);
+			
 		}else{
-			res.json({ response: 400});
+			res.send(response);
+		}
+	})
+});
+
+router.get('/api/initialWebPage', function(req, res) {
+	request(initialWebPage, function (error, response, body) {
+		if (!error && response.statusCode == 200 && response.headers["set-cookie"][0]!== undefined) {
+			http_session = response.headers["set-cookie"][0];
+			headers["set-cookie"] = "";
+			headers["set-cookie"] = http_session+si_session+si_security;
+			res.send(response);
+		}
+		else{
+			res.send(response);
+		}
+	})
+});
+
+router.get('/api/userpage', function(req, res) {	
+	getStudentPage.form.pct_id = req.body.userid;
+	// Start the request	
+	request(getStudentPage, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			res.send(response);
+		}
+		else{
+			res.send(response);
 		}
 	})
 });
