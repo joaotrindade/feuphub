f9081706d652fdddfe6c950286a20b707a46b5db
joaotrindade <joao.ferreira.trindade@gmail.com>
2014-11-04 00:03:36 +0000

    Error fix

--- database/cadeira.js
+++ database/cadeira.js
@@ -2,7 +2,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
-  user     : 'feuphubmain',
+  user     : 'feuphub',
   password : 'chituc2014',
   database : 'feuphubmain'
 });
