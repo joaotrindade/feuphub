2546860d788eaf47e66ef1147f531feef51bc36f
Pedro Silva <pedro.cls93@gmail.com>
2014-11-05 14:52:43 +0000

    2nd jenkins test

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
-	  //comments
+	  //comment
     })
   })
 })
