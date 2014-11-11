64e6d4e1b7e6af38e97c0d8e853aaf3541890a8a
ruipneves <ruiwp_93@hotmail.com>
2014-11-11 01:15:26 +0000

    Mudança do login da BD

--- database/resposta.js
+++ database/resposta.js
@@ -2,8 +2,8 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
-  user     : 'root',
-  password : '',
+  user     : 'feuphub',
+  password : 'chituc2014',
   database : 'feuphubmain'
 });
 
