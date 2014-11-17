ede90f405fa7d005c11bd525130f4b035e44bf91
ruipneves <ruiwp_93@hotmail.com>
2014-11-17 18:30:39 +0000

    UPVOTE/DOWNVOTE management.

--- server/Database/topico.js
+++ server/Database/topico.js
@@ -21,6 +21,104 @@ module.exports = (function() {
 		});
 	};
 	
+	function upvoteTopico(uID,tID,callback) {
+		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Topico WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results1)
+		{
+			if(results1[0].nr == 1)
+			{
+				connection.query("SELECT upvote,downvote FROM Utilizador_Topico WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results2)
+				{
+					if(results2[0].upvote == 1)
+					{
+						connection.query("UPDATE Topico SET upvote=upvote-1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Topico SET upvote=0 WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else if(results2[0].downvote == 1)
+					{
+						connection.query("UPDATE Topico SET downvote=downvote-1,upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Topico SET upvote=1,downvote=0 WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else
+					{
+						connection.query("UPDATE Topico SET upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Topico SET upvote=1 WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+				});
+			}
+			else
+			{
+				connection.query("INSERT INTO Utilizador_Topico VALUES(" + uID + "," + tID + ",1,0)", function(err, results)
+				{
+					callback(err,results);
+				});
+			}
+		});
+	}
+	
+	function downvoteTopico(uID,tID,callback) {
+		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Topico WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results1)
+		{
+			if(results1[0].nr == 1)
+			{
+				connection.query("SELECT upvote,downvote FROM Utilizador_Topico WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results2)
+				{
+					if(results2[0].downvote == 1)
+					{
+						connection.query("UPDATE Topico SET downvote=downvote-1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Topico SET downvote=0 WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else if(results2[0].upvote == 1)
+					{
+						connection.query("UPDATE Topico SET upvote=upvote-1,downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Topico SET downvote=1,upvote=0 WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else
+					{
+						connection.query("UPDATE Topico SET downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Topico SET downvote=1 WHERE UtilizadorKey = " + uID + " AND TopicoKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+				});
+			}
+			else
+			{
+				connection.query("INSERT INTO Utilizador_Topico VALUES(" + uID + "," + tID + ",0,1)", function(err, results)
+				{
+					callback(err,results);
+				});
+			}
+		});
+	}
+	
 	api.post('/:courseID', function(req, res) {
 		var cID = req.params.courseID;
 		getTopicos(cID,function(err,result) {
@@ -61,6 +159,56 @@ module.exports = (function() {
 				});
 			}
 		});
+});
+
+	api.post('/:typeID/:topicID', function(req, res) {
+		console.log("aqui");
+		var body = req.body,
+		  token = body.token,
+		  uID = body.idUser,
+		  type = req.params.typeID,
+		  tID = req.params.topicID;
+		  console.log(type);
+		  console.log(uID);
+		  console.log(tID);
+		  if(type=="up") {
+				upvoteTopico(uID,tID,function(err,result) {
+					if(err)
+					{
+						console.log(err);
+						res.send({
+							success: false,
+							results: err
+						});
+					}
+					else {
+						console.log(result);
+						res.send({
+							success: true,
+							results: result
+						});
+					}
+				});
+		  }
+		  else if(type=="down") {
+				downvoteTopico(uID,tID,function(err,result) {
+				if(err)
+				{
+					console.log(err);
+					res.send({
+						success: false,
+						results: err
+					});
+				}
+				else {
+					console.log(result);
+					res.send({
+						success: true,
+						results: result
+					});
+				}
+			});
+		  }
 });
    
     return {api: api, start: start};
