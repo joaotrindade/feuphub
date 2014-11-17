6a69278c1e6defb6764cbfafad3ce194ffb4a859
ruipneves <ruiwp_93@hotmail.com>
2014-11-17 19:14:58 +0000

    UPVOTE/DOWNVOTE para respostas

--- server/Database/resposta.js
+++ server/Database/resposta.js
@@ -21,6 +21,110 @@ module.exports = (function() {
 		});
 	};
 	
+	function upvoteResposta(uID,tID,callback) {
+		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results1)
+		{
+			if(results1[0].nr == 1)
+			{
+				connection.query("SELECT upvote,downvote FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results2)
+				{
+					if(results2[0].upvote == 1)
+					{
+						connection.query("UPDATE Resposta SET upvote=upvote-1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Resposta SET upvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else if(results2[0].downvote == 1)
+					{
+						connection.query("UPDATE Resposta SET downvote=downvote-1,upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Resposta SET upvote=1,downvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else
+					{
+						connection.query("UPDATE Resposta SET upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Resposta SET upvote=1 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+				});
+			}
+			else
+			{
+				connection.query("INSERT INTO Utilizador_Resposta VALUES(" + uID + "," + tID + ",1,0)", function(err, results)
+				{
+					connection.query("UPDATE Resposta SET upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+					{
+						callback(err,results);
+					});
+				});
+			}
+		});
+	}
+	
+	function downvoteResposta(uID,tID,callback) {
+		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results1)
+		{
+			if(results1[0].nr == 1)
+			{
+				connection.query("SELECT upvote,downvote FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results2)
+				{
+					if(results2[0].downvote == 1)
+					{
+						connection.query("UPDATE Resposta SET downvote=downvote-1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Resposta SET downvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else if(results2[0].upvote == 1)
+					{
+						connection.query("UPDATE Resposta SET upvote=upvote-1,downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Resposta SET downvote=1,upvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+					else
+					{
+						connection.query("UPDATE Resposta SET downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+						{
+							connection.query("UPDATE Utilizador_Resposta SET downvote=1 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							{
+								callback(err,results);
+							});
+						});
+					}
+				});
+			}
+			else
+			{
+				connection.query("INSERT INTO Utilizador_Resposta VALUES(" + uID + "," + tID + ",0,1)", function(err, results)
+				{
+					connection.query("UPDATE Resposta SET downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+					{
+						callback(err,results);
+					});
+				});
+			}
+		});
+	}
+	
 	api.post('/', function(req, res) {
 	if (auth.validTokenProvided(req, res)) {
 		var id = req.body.id_questao;
@@ -64,6 +168,53 @@ api.post('/:topicID', function(req, res) {
 				});
 			}
 		});
+});
+
+api.post('/:typeID/:answerID', function(req, res) {
+		if (auth.validTokenProvided(req, res)) {
+			var body = req.body,
+			  uID = body.idUser,
+			  type = req.params.typeID,
+			  rID = req.params.answerID;
+			  if(type=="up") {
+					upvoteResposta(uID,rID,function(err,result) {
+						if(err)
+						{
+							console.log(err);
+							res.send({
+								success: false,
+								results: err
+							});
+						}
+						else {
+							console.log(result);
+							res.send({
+								success: true,
+								results: result
+							});
+						}
+					});
+			  }
+			  else if(type=="down") {
+					downvoteResposta(uID,rID,function(err,result) {
+					if(err)
+					{
+						console.log(err);
+						res.send({
+							success: false,
+							results: err
+						});
+					}
+					else {
+						console.log(result);
+						res.send({
+							success: true,
+							results: result
+						});
+					}
+				});
+			  }
+		}
 });
    
     return {api: api, start: start};
