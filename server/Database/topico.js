24ae01fbfe0c03342268ef25d724b3a16496891d
ruipneves <ruiwp_93@hotmail.com>
2014-12-01 15:03:27 +0000

    Arranjar o delete dos feedbacks às respostas

--- server/Database/topico.js
+++ server/Database/topico.js
@@ -19,8 +19,7 @@ module.exports = (function() {
 		{
 			for(var x=0;x<resultssel.length;x++)
 			{
-				console.log(resultssel[x]);
-				connection.query("DELETE FROM Utilizador_Resposta inner WHERE RespostaKey=" + resultssel[x], function(err, results)
+				connection.query("DELETE FROM Utilizador_Resposta inner WHERE RespostaKey=" + resultssel[x].RespostaKey, function(err, results)
 				{
 					
 				});
