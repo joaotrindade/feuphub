a8b755f082cf1ac6972cb38d2eec282f93f07dc2
Paulo Bordalo Marcos <paulosuperd@hotmail.com>
2014-12-01 15:18:29 +0000

    Redirect estava com codigo em Maiusculas

--- application/js/app.js
+++ application/js/app.js
@@ -562,7 +562,7 @@ App.TopicController = Ember.ObjectController.extend({
 				  if (response.success)
 				  {
 						alert("Topico Eliminado");
-						self.transitionToRoute('cursos',{queryParams: {codigo: self.topicoDetails.CursoKey}});
+						self.transitionToRoute('cursos',{queryParams: {codigo: self.topicoDetails.CursoKey.toLowerCase()}});
 				  }
 				  else
 						alert("ALGO DEU MAL A APAGAR O TOPICO");
