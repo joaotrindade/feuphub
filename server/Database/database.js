fc441acad31c41134376beda3a2acd3ae8646870
João Trindade <joao.ferreira.trindade@gmail.com>
2014-12-02 09:50:37 +0000

    Database changed

--- server/Database/database.js
+++ server/Database/database.js
@@ -5,7 +5,7 @@ module.exports = (function() {
 		host     : '127.0.0.1',
 		user     : 'feuphub',
 		password : 'chituc2014',
-		database : 'feuphubalt'
+		database : 'feuphubmain'
 	});
 	
 	var api = express.Router();
