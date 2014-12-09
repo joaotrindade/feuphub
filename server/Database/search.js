38d8157c67dbf4b710e2132461c3fd5b8b7a77a4
Pedro Silva <pedro.cls93@gmail.com>
2014-12-09 14:38:20 +0000

    Changed sql query for faster search resolution

--- server/Database/search.js
+++ server/Database/search.js
@@ -14,29 +14,91 @@ module.exports = (function() {
 		connection = conn;
 	};
 	
-	function searchInDatabase(string, authenticated){
+	function searchInDatabase(string, username){
 		var deferred = Q.defer();
 
 		var regex = /[a-z]+/gi;
 		var array = string.match(regex);
 
 		var searchResult = {};
+		if(username){
+			searchTopicoAutenticado(array,username).then(function (result){
+				searchResult['topicos']=result; 
+				return searchCurso(array);
+			}).then(function (result){
+				searchResult['cursos']=result; 
+				return searchCadeiraAutenticado(array,username);		
+			}).then(function(result){
+				searchResult['cadeiras']=result;
+				deferred.resolve(searchResult);
+			}).catch(console.log).done();
+		}else{
+			searchTopico(array).then(function (result){
+				searchResult['topicos']=result; 
+				return searchCurso(array);
+			}).then(function (result){
+				searchResult['cursos']=result; 
+				deferred.resolve(searchResult);
+			}).catch(console.log).done();
+		}
+		return deferred.promise;
+	}
+	
+	function searchTopicoAutenticado(array, username){
+		//nao falta parte nao autenticada?
+		var deferred = Q.defer();
+		var stringQuery = "SELECT `id`,`titulo` From `Topico` INNER JOIN `CadeirasConcluidas` ON `CadeirasConcluidas.CadeiraKey`=`Topico.CadeiraKey` WHERE `CadeirasConcluidas.UtilizadorKey`= ' "+username+"'";
+		for(var i=0; i<array.length;i++)
+		{
+			if(i+1==array.length){
+				stringQuery += "`titulo` like '%"+array[i]+"%'";			
+			}else{
+				stringQuery += "`titulo` like '%"+array[i]+"%' OR ";
+			}
+		}
 		
-		searchTopico(array,authenticated).then(function (result){
-			searchResult['topicos']=result; 
-			return searchCurso(array);
-		}).then(function (result){
-			searchResult['cursos']=result; 
-			return searchCadeira(array,authenticated);
-		}).then(function(result){
-			searchResult['cadeiras']=result;
-			deferred.resolve(searchResult);
-		}).catch(console.log).done();
+		//console.log("searchTopicoAutenticado: "+stringQuery);
+		//console.log("\n");
 		
+		connection.query(stringQuery, function(err, result){
+			if(!err){
+				//console.log(result);
+				//console.log("\n\n");
+				deferred.resolve(result);	
+			}
+		});
+
 		return deferred.promise;
 	}
 
-	function searchTopico(array, authenticated){
+	function searchCadeiraAutenticado(array,username){
+		var deferred = Q.defer();
+		var stringQuery = "SELECT `sigla`, `nome` FROM `Cadeira` INNER JOIN `CadeirasConcluidas` ON `CadeirasConcluidas.CadeiraKey`=`Cadeira.codigo` WHERE `CadeirasConcluidas.UtilizadorKey`= ' "+username+"'";
+		
+		for(var i=0; i<array.length;i++)
+		{
+			if(i+1==array.length){
+				stringQuery += "`nome` like '%"+array[i]+"%' OR `sigla` like '%"+array[i]+"%';";
+			}else{
+				stringQuery += "`nome` like '%"+array[i]+"%' OR `sigla` like '%"+array[i]+"%' OR ";
+			}
+		}
+		
+		//console.log("searchCadeiraAutenticado: "+stringQuery);
+		//console.log("\n");
+		
+		connection.query(stringQuery, function(err, result){
+			if(!err){
+				//console.log(result);
+				//console.log("\n\n");
+				deferred.resolve(result);	
+			}
+		});
+		
+		return deferred.promise;
+	}
+	
+	function searchTopico(array){
 	
 		var deferred = Q.defer();
 		var stringQuery = "Select `id`,`titulo` From Topico Where ";
@@ -49,41 +111,7 @@ module.exports = (function() {
 				stringQuery += "`titulo` like '%"+array[i]+"%' OR ";
 			}
 		}
-		var authenticatedResults = [];
-		
-		if(authenticated){
-			//get user cadeiras
-			//for each check the topic.title where cadeira.key = user.cadeira.key
-			var userCadeiras = "SELECT `CadeiraKey` FROM `CadeirasConcluidas` WHERE `UtilizadorKey` = '"+authenticated+"'";
-			connection.query(userCadeiras, function(err,result){
-				if(err)
-					deferred.reject();
-				else{
-					result.forEach(function(cadeiraKey){
-						var topicos = "Select `id`,`titulo` From Topico Where `CadeiraKey` = '"+cadeiraKey+"'";
-						if(array.length > 0){
-							topicos +=" AND "
-							for(var i=0; i<array.length;i++)
-							{
-								if(i+1==array.length){
-									topicos += "`titulo` like '%"+array[i]+"%'";			
-								}else{
-									topicos += "`titulo` like '%"+array[i]+"%' OR ";
-								}
-							}
-						}
-						topicos +=";";
-						connection.query(topicos, function(err,result){
-							if(!err){
-								authenticatedResults.push.apply(authenticatedResults,result);
-							}
-						});
-					});
-				}
-			});
-		}
-		
-		
+				
 		//console.log("searchTopico: "+stringQuery);
 		//console.log("\n");
 		
@@ -91,7 +119,6 @@ module.exports = (function() {
 			if(!err){
 				//console.log(result);
 				//console.log("\n\n");
-				result.push.apply(result,authenticatedResults);
 				deferred.resolve(result);
 			}
 		});
@@ -125,84 +152,29 @@ module.exports = (function() {
 		});
 		return deferred.promise;
 	}
-
-	function searchCadeira(array, authenticated){
-		
-		var deferred = Q.defer();
-		if(authenticated){
-			var authenticatedResults = [];
-			var stringQuery;
-			var userCadeiras = "SELECT `CadeiraKey` FROM `CadeirasConcluidas` WHERE `UtilizadorKey` = '"+authenticated+"'";
-			connection.query(userCadeiras, function(err,result){
-				if(err)
-					deferred.reject();
-				else{
-					result.forEach(function(cadeiraKey){
-					stringQuery = "Select `sigla`,`nome` From Cadeira Where `codigo` = '"+cadeiraKey+"'";
-					if(array.length > 0){
-						stringQuery +=" AND "
-						for(var i=0; i<array.length;i++)
-						{
-							if(i+1==array.length){
-								stringQuery += "`nome` like '%"+array[i]+"%' OR `sigla` like '%"+array[i]+"%';";
-							}else{
-								stringQuery += "`nome` like '%"+array[i]+"%' OR `sigla` like '%"+array[i]+"%' OR ";
-							}
-						}
-					}
-					stringQuery +=";";
-
-					connection.query(stringQuery, function(err, result){
-						if(!err){
-							//console.log(result);
-							//console.log("\n\n");
-							authenticatedResults.push.apply(authenticatedResults,result);
-						}
-					});
-					});
-				}
-			});
-			deferred.resolve(result);	
-			//console.log("searchCadeira: "+stringQuery);
-			//console.log("\n");
-
-		}
-		else{
-			deferred.resolve([]);
-		}
-		
-		return deferred.promise;
-	}
 	
 	api.post('/', function(req, res) {
 		var body = req.body, username = body.type, string = body.query;		
 
-		if(username) //username defined
-		{
-			console.log(username);
-			if (auth.validTokenProvided(req, res)) {
-				
-			}
-			else{
-				searchInDatabase(string).then(function(searchResult){
-					res.send({
-						success: true,
-						results: searchResult
-					});
-				}).catch(function(err){
-					res.send({
-						success: false,
-						results: err
-					});
+		if (auth.validTokenProvided(req, res)) {
+			searchInDatabase(string, username).then(function(searchResult){
+				res.send({
+					success: true,
+					results: searchResult
 				});
-			}
+			}).catch(function(err){
+				res.send({
+					success: false,
+					results: err
+				});
+			});
 		}
 		else{
 			searchInDatabase(string).then(function(searchResult){
 				res.send({
 					success: true,
-					response: searchResult
-				})
+					results: searchResult
+				});
 			}).catch(function(err){
 				res.send({
 					success: false,
