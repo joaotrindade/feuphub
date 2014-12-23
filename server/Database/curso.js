6b8bcb49514239eb9ef5525723c67fa100e40460
joaotrindade <joao.ferreira.trindade@gmail.com>
2014-12-23 05:36:00 +0000

    adicionado get stats de curso

--- server/Database/curso.js
+++ server/Database/curso.js
@@ -21,6 +21,16 @@ module.exports = (function() {
 		});
 	}
 	
+	function getStats(idCurso, callback){
+		var resfinal = {};
+		connection.query("SELECT * FROM (select count(*) as n_positivos from Feedback where avaliacao=true and CursoKey="+connection.escape(idCurso)+")db UNION ALL SELECT * FROM(select count(*)as cenas from Feedback where CursoKey="+connection.escape(idCurso)+")db2 ;", function(err2, results2)
+		{
+			resfinal.positivos = results2[0];
+			resfinal.total = results2[1];
+			callback(err2,resfinal);
+		});
+	}
+	
     api.get('/', function(req, res) {
         getAll(function(err,results)
 		{
@@ -60,6 +70,23 @@ module.exports = (function() {
 	api.get('/:sigla', function(req, res) {
         var sigla = req.params.sigla;
 		getOne(sigla,function(err,results)
+		{
+			if(err)
+				res.send({
+					success: false,
+					results: err
+				});
+			else
+				res.send({
+					success: true,
+					results: results
+				});	
+		});
+    });
+	
+	api.get('/stats/:sigla', function(req, res) {
+        var sigla = req.params.sigla;
+		getStats(sigla,function(err,results)
 		{
 			if(err)
 				res.send({
