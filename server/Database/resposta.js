1d8b75da18519c03600ddf2d525b9358f070c9b2
joaotrindade <joao.ferreira.trindade@gmail.com>
2014-12-23 02:39:34 +0000

    sanitizing and escaping text fields resposta api

--- server/Database/resposta.js
+++ server/Database/resposta.js
@@ -8,31 +8,31 @@ module.exports = (function() {
 	};
 	
 	function addOne(idn,texton,datan,user_id,callback){
-		connection.query("INSERT INTO Resposta(upvote,downvote,texto,data,UtilizadorKey,TopicoKey) VALUES(0,0,'" + texton + "',CURRENT_TIMESTAMP()," + user_id + "," + idn + ")", function(err, results)
+		connection.query("INSERT INTO Resposta(upvote,downvote,texto,data,UtilizadorKey,TopicoKey) VALUES(0,0,'" + connection.escape(texton) + "',CURRENT_TIMESTAMP()," + connection.escape(user_id) + "," + connection.escape(idn) + ")", function(err, results)
 		{
 			callback(err,results);
 		});
 	};
 	
 	function getRespostas(tID,callback){
-		connection.query("SELECT Resposta.id,upvote-downvote as difference,texto,DATE_FORMAT(data,'%h:%i %p %M %e, %Y') as data,nickname as nome FROM Resposta inner join Utilizador on Resposta.UtilizadorKey = Utilizador.numero inner join Visitante on Utilizador.VisitanteKey = Visitante.id WHERE TopicoKey = " + tID + " ORDER BY difference desc", function(err, results)
+		connection.query("SELECT Resposta.id,upvote-downvote as difference,texto,DATE_FORMAT(data,'%h:%i %p %M %e, %Y') as data,nickname as nome FROM Resposta inner join Utilizador on Resposta.UtilizadorKey = Utilizador.numero inner join Visitante on Utilizador.VisitanteKey = Visitante.id WHERE TopicoKey = " + connection.escape(tID) + " ORDER BY difference desc", function(err, results)
 		{
 			callback(err,results);
 		});
 	};
 	
 	function upvoteResposta(uID,tID,callback) {
-		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results1)
+		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Resposta WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results1)
 		{
 			if(results1[0].nr == 1)
 			{
-				connection.query("SELECT upvote,downvote FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results2)
+				connection.query("SELECT upvote,downvote FROM Utilizador_Resposta WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results2)
 				{
 					if(results2[0].upvote == 1)
 					{
-						connection.query("UPDATE Resposta SET upvote=upvote-1 WHERE id = " + tID + "", function(err, results)
+						connection.query("UPDATE Resposta SET upvote=upvote-1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 						{
-							connection.query("UPDATE Utilizador_Resposta SET upvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							connection.query("UPDATE Utilizador_Resposta SET upvote=0 WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results3)
 							{
 								results['tipo'] = "retirou";
 								callback(err,results);
@@ -41,9 +41,9 @@ module.exports = (function() {
 					}
 					else if(results2[0].downvote == 1)
 					{
-						connection.query("UPDATE Resposta SET downvote=downvote-1,upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+						connection.query("UPDATE Resposta SET downvote=downvote-1,upvote=upvote+1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 						{
-							connection.query("UPDATE Utilizador_Resposta SET upvote=1,downvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							connection.query("UPDATE Utilizador_Resposta SET upvote=1,downvote=0 WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results3)
 							{
 								results['tipo'] = "trocou";
 								callback(err,results);
@@ -52,9 +52,9 @@ module.exports = (function() {
 					}
 					else
 					{
-						connection.query("UPDATE Resposta SET upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+						connection.query("UPDATE Resposta SET upvote=upvote+1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 						{
-							connection.query("UPDATE Utilizador_Resposta SET upvote=1 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							connection.query("UPDATE Utilizador_Resposta SET upvote=1 WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results3)
 							{
 								results['tipo'] = "inseriu";
 								callback(err,results);
@@ -65,9 +65,9 @@ module.exports = (function() {
 			}
 			else
 			{
-				connection.query("INSERT INTO Utilizador_Resposta VALUES(" + uID + "," + tID + ",0,1)", function(err, results)
+				connection.query("INSERT INTO Utilizador_Resposta VALUES(" + connection.escape(uID) + "," + connection.escape(tID) + ",0,1)", function(err, results)
 				{
-					connection.query("UPDATE Resposta SET upvote=upvote+1 WHERE id = " + tID + "", function(err, results)
+					connection.query("UPDATE Resposta SET upvote=upvote+1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 					{
 						results['tipo'] = "inseriu";
 						callback(err,results);
@@ -76,19 +76,19 @@ module.exports = (function() {
 			}
 		});
 	}
-	
+	 
 	function downvoteResposta(uID,tID,callback) {
-		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results1)
+		connection.query("SELECT COUNT(UtilizadorKey) as nr FROM Utilizador_Resposta WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results1)
 		{
 			if(results1[0].nr == 1)
 			{
-				connection.query("SELECT upvote,downvote FROM Utilizador_Resposta WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results2)
+				connection.query("SELECT upvote,downvote FROM Utilizador_Resposta WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results2)
 				{
 					if(results2[0].downvote == 1)
 					{
-						connection.query("UPDATE Resposta SET downvote=downvote-1 WHERE id = " + tID + "", function(err, results)
+						connection.query("UPDATE Resposta SET downvote=downvote-1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 						{
-							connection.query("UPDATE Utilizador_Resposta SET downvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							connection.query("UPDATE Utilizador_Resposta SET downvote=0 WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results3)
 							{
 								results['tipo'] = "retirou";
 								callback(err,results);
@@ -97,9 +97,9 @@ module.exports = (function() {
 					}
 					else if(results2[0].upvote == 1)
 					{
-						connection.query("UPDATE Resposta SET upvote=upvote-1,downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+						connection.query("UPDATE Resposta SET upvote=upvote-1,downvote=downvote+1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 						{
-							connection.query("UPDATE Utilizador_Resposta SET downvote=1,upvote=0 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							connection.query("UPDATE Utilizador_Resposta SET downvote=1,upvote=0 WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results3)
 							{
 								results['tipo'] = "trocou";
 								callback(err,results);
@@ -108,9 +108,9 @@ module.exports = (function() {
 					}
 					else
 					{
-						connection.query("UPDATE Resposta SET downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+						connection.query("UPDATE Resposta SET downvote=downvote+1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 						{
-							connection.query("UPDATE Utilizador_Resposta SET downvote=1 WHERE UtilizadorKey = " + uID + " AND RespostaKey = " + tID + "", function(err, results3)
+							connection.query("UPDATE Utilizador_Resposta SET downvote=1 WHERE UtilizadorKey = " + connection.escape(uID) + " AND RespostaKey = " + connection.escape(tID) + "", function(err, results3)
 							{
 								results['tipo'] = "inseriu";
 								callback(err,results);
@@ -121,9 +121,9 @@ module.exports = (function() {
 			}
 			else
 			{
-				connection.query("INSERT INTO Utilizador_Resposta VALUES(" + uID + "," + tID + ",1,0)", function(err, results)
+				connection.query("INSERT INTO Utilizador_Resposta VALUES(" + connection.escape(uID) + "," + connection.escape(tID) + ",1,0)", function(err, results)
 				{
-					connection.query("UPDATE Resposta SET downvote=downvote+1 WHERE id = " + tID + "", function(err, results)
+					connection.query("UPDATE Resposta SET downvote=downvote+1 WHERE id = " + connection.escape(tID) + "", function(err, results)
 					{
 						results['tipo'] = "inseriu";
 						callback(err,results);
