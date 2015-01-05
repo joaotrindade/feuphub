164fc373e41068ba74e6a30f81dead50fa192730
Pedro Silva <pedro.cls93@gmail.com>
2015-01-05 22:12:31 +0000

    firefox bug 3

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ else{
 var options = {
   key: fs.readFileSync('./server/opt/certs/feuphub_fe_up_pt.key'),
   cert: fs.readFileSync('./server/opt/certs/cert-437-feuphub.fe.up.pt.pem'),
-  chain: fs.readFileSync('./server/opt/certs/chain-437-feuphub.fe.up.pt.pem'),
+  ca: fs.readFileSync('./server/opt/certs/chain-437-feuphub.fe.up.pt.pem'),
   missinglink: fs.readFileSync('./server/opt/certs/TERENASSLCA.crt')
 };
 
